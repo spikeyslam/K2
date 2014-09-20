dc2bc7e7c771aaf86c9df24682595dc810bb0af5
Steve Rabins-Lam <spikeyslam@gmail.com>
2014-09-19 20:38:58 -0700

    added font-awesome

--- header.php
+++ header.php
@@ -20,6 +20,8 @@ defined( 'K2_CURRENT' ) or die ( __('Error: This file can not be loaded directly
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 
 <?php if ( is_singular() ): ?>
